dba000b2580a9e9ed7e629223346c68a1a0db944
J C C Thomas <jthomas48@argon-login-6.hpc>
2026-02-12 12:53:44 -0600

    comments, no pyvista on hpc, need to run thru container?

--- tools/decimator.sh
+++ tools/decimator.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+#designed to be used like
+# ./decimator.sh /Shared/gb_lss/Thomas/iowaRme/fullScans/pat058/pat058l_01.ply /Shared/gb_lss/Thomas/iowaRme/dec016Scans/pat058/pat058l_01.ply
+#dont forget to make it into a unix file and make it executable 
+#CURRENTLY NOT WORKING ON HPC BC THEY DONT HAVE PYVISTA
+#NEED TO SEE IF I CAN RUN IT SOMEWHERE ELSE, LOCALLY?
+
 # Check if two arguments are provided
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <file1> <file2>"
