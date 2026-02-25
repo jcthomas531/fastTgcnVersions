91c44fc849854d67c742dbd4b09dd576f2e5a2db
J C C Thomas <jthomas48@argon-login-6.hpc>
2026-02-24 18:44:16 -0600

    visualizing new predictions

--- prediction/viewingPreds.py
+++ prediction/viewingPreds.py
@@ -7,8 +7,41 @@ import os
 
 
 #RME prediction
-os.chdir("K:/iowaRme/test2Pred/")
-pf.readAndPlot(file = "convFunTestDec.ply", 
+os.chdir("K:/iowaRme/test1PredA/")
+pf.readAndPlot(file = "decimIowa.ply", 
+               arch = "U")
+os.chdir("K:/iowaRme/test1PredA/")
+pf.readAndPlot(file = "001_U.ply", 
+               arch = "U")
+
+os.chdir("K:/iowaRme/test1PredB/")
+pf.readAndPlot(file = "decimIowa.ply", 
+               arch = "U")
+os.chdir("K:/iowaRme/test1PredB/")
+pf.readAndPlot(file = "001_U.ply", 
+               arch = "U")
+
+
+os.chdir("K:/iowaRme/test1PredC/")
+pf.readAndPlot(file = "decimIowa.ply", 
+               arch = "U")
+os.chdir("K:/iowaRme/test1PredC/")
+pf.readAndPlot(file = "001_U.ply", 
+               arch = "U")
+pf.readAndPlot(file = "Y:/dissModels/fastTgcnVersions/fastTgcnEasy/modelOutputs/2026_01_27 full upper/pred_global/001_U.ply", 
+               arch = "U")
+#not exactly the same but pretty darn close
+
+
+
+os.chdir("K:/iowaRme/test1PredD/")
+pf.readAndPlot(file = "decimIowa.ply", 
+               arch = "U")
+os.chdir("K:/iowaRme/test1PredD/")
+pf.readAndPlot(file = "001_U.ply", 
+               arch = "U")
+os.chdir("K:/iowaRme/test1PredD/")
+pf.readAndPlot(file = "decimIowaRegistered.ply", 
                arch = "U")
 
 
@@ -27,7 +60,7 @@ pf.readAndPlot(file = "001_U.ply",
 
 import pyvista as pv
 dargs = dict(show_edges=False, rgb=True)
-m1 = pv.read("K:/iowaRme/test2Pred/convFunTestDec.ply")
+m1 = pv.read("K:/iowaRme/test1Pred/decimIowa.ply")
 m2 = pv.read("K:/iowaRme/test1Pred/001_U.ply")
 m3 = pv.read("Y:/dissModels/fastTgcnVersions/fastTgcnEasy/modelOutputs/2026_01_27 full upper/pred_global/001_U.ply")
 
