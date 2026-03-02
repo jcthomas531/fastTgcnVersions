78ab6a9a8027148b9fd764dce1b54edf57b71eb3
J C C Thomas <jthomas48@argon-login-6.hpc>
2026-03-02 08:28:33 -0600

    prediction clean up

--- prediction/predPipe2PatchedRunner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l ngpus=3
-#$ -pe smp 16
-#$ -o outputFiles/$JOB_ID_predOutPatched.o
-#$ -e outputFiles/$JOB_ID_predErrorPatched.e
-
-
-
-apptainer exec ../../../containers/pytorch2.sif python predPipe2Patched.py
--- prediction/predRunner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l ngpus=3
-#$ -pe smp 16
-#$ -o outputFiles/$JOB_ID_predOut.o
-#$ -e outputFiles/$JOB_ID_predError.e
-
-
-
-apptainer exec ../../../containers/pytorch2.sif python predPipe2.py
