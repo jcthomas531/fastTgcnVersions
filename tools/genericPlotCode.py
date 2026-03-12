bc86cc0a7391231ed4fc94e50846c17985d6ad6b
J C C Thomas <jthomas48@argon-itf-login-4.hpc>
2026-03-12 18:41:16 -0500

    registration folder and reorganization

--- tools/genericPlotCode.py
+++ tools/genericPlotCode.py
@@ -5,8 +5,19 @@ import os
 
 
 
-dir1 = "K:/teeth3DS/scanData/upper/"
-pf.readAndPlot(dir1 + "6X24ILNE/6X24ILNE_U.ply", arch = "U")
+dir1 = "Y:/dissModels/fastTgcnVersions/fastTgcnEasy/modelOutputs/2026_03_12 warmstartTest1/pred_global"
+os.chdir(dir1)
+aaa = os.listdir()
+for i in range(len(aaa)):
+    pf.readAndPlot(aaa[i], "U")
+    
+dir2 = "K:/testDir/warmStartTestData/train" 
+os.chdir(dir2)
+aaa = os.listdir()
+for i in range(len(aaa)):
+    pf.readAndPlot(aaa[i], "U")
+
+pf.readAndPlot("01A91JH6_UDecim016.ply", arch = "U")
 pf.readAndPlot(dir1 + "6X24ILNE/6X24ILNE_UDecim016.ply", arch = "U")
 
 dir2 = "K:/IOSSegData/clean/testCleanU/"
