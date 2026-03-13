092c416ec035bcf6b712f694d7608a21821edbbe
J C C Thomas <jthomas48@argon-login-5.hpc>
2026-03-13 10:41:42 -0500

    registration exporting and pipeline

--- tools/registration/registerScans.py
+++ tools/registration/registerScans.py
@@ -1,42 +1,57 @@
 import sys
 sys.path.append("Y:/dissModels/fastTgcnVersions/tools/registration")
-import registrationFuns as regi
+import registrationFuns as rg
 import os
 import open3d as o3d
 import copy
+import trimesh
+import numpy as np
+
+import random
+random.seed(826)
 
 
-#bring in an arbitrary iosseg scan to use as a target for registration
-iossegDir = "K:/IOSSegData/clean/trainCleanU/"
-regTarget = o3d.io.read_point_cloud(iossegDir + "007_U.ply")
+#target file
+iossegPath = "K:/IOSSegData/clean/trainCleanU/007_U.ply"
+targetCloud = o3d.io.read_point_cloud(iossegPath)
 
 
-#to start lets grab an arbitrary scan from the warmstartTest data
-#this will be the source, what is being transformed
-testDir = "K:/testDir/warmstartTestDataReg/"
-os.chdir(testDir)
-regSource = o3d.io.read_point_cloud("train/00OMSZGW_UDecim016.ply")
+#loop for train files
+outDirTrain = "K:/testDir/warmstartTestDataReg/train/"
+inDirTrain = "K:/testDir/warmstartTestData/train/"
+trainToReg = os.listdir(inDirTrain)
+for i in range(len(trainToReg)):
+    #obtaining transformation
+    sourceCloudi = o3d.io.read_point_cloud(inDirTrain + trainToReg[i])
+    regi = rg.getRegistration(source = sourceCloudi, target=targetCloud)
+    #register the meshes and export
+    rg.registerAndExport(inFile = inDirTrain + trainToReg[i],
+                         outFile = outDirTrain + trainToReg[i],
+                         trans = regi.transformation) 
+    print("Done: " + str(i))
 
 
-#
-#plot how it looks prior to transformation (with actual colors)
-o3d.visualization.draw_geometries([regSource, regTarget])
-regi.monochromePlot(regSource, regTarget)
-#calculate registration
-reg1 = regi.getRegistration(source = regSource, target=regTarget)
-#look at stats on the registration performance
-reg1
-#look at transformation matri
-reg1.transformation
-#transform source to align with target
-regSourceTrans = regSource.transform(reg1.transformation)
-#plot registered point clouds
-regi.monochromePlot(regSourceTrans, regTarget)
+
+#loop for test files
+outDirTest = "K:/testDir/warmstartTestDataReg/test/"
+inDirTest = "K:/testDir/warmstartTestData/test/"
+testToReg = os.listdir(inDirTest)
+for i in range(len(testToReg)):
+    #obtaining transformation
+    sourceCloudi = o3d.io.read_point_cloud(inDirTest + testToReg[i])
+    regi = rg.getRegistration(source = sourceCloudi, target=targetCloud)
+    #register the meshes and export
+    rg.registerAndExport(inFile = inDirTest + testToReg[i],
+                         outFile = outDirTest + testToReg[i],
+                         trans = regi.transformation) 
+    print("Done: " + str(i))
 
 
-#though not perfect bc these are two different scans, they are pointing in the same
-#direction. One question about this is does this transformation allow for shrinking?
-#is that something we would want or not want? I would think not want unless on different scales..
-#also there are probably features we can tweak to make the fit a bit more general since
-#these are two different arches but this is good for now.
-#now we must export it and make sure its format is right ugh i really need to streamline the process
+
+
+
+
+
+
+
+
--- tools/registration/registrationFuns.py
+++ tools/registration/registrationFuns.py
@@ -1,20 +1,24 @@
 import os
 import open3d as o3d
 import copy
-
+import trimesh
+import numpy as np
 
 #largely stolen from these tutorials
 #https://www.open3d.org/docs/0.7.0/tutorial/Basic/icp_registration.html
 #https://www.open3d.org/docs/0.7.0/tutorial/Advanced/global_registration.html#global-registration
 #see my notes in registrationLearning2.py for more details on the functions
 #to apply a transformation use pointCloud.transform(transMat)
+#open3d will not export face colors, must use trimesh
 
 #NOTE
 #there seems to be something non-deterministic about this process, set seed when using
 
 
 
-
+###############################################################################
+#MONOCHROME PLOTTING
+###############################################################################
 #plot both point clouds, slight moditication of draw_registration_result from tutorial
 #takes two point clouds, source and target
 def monochromePlot(source, target):
@@ -24,7 +28,10 @@ def monochromePlot(source, target):
     target_temp.paint_uniform_color([0, 0.651, 0.929])
     o3d.visualization.draw_geometries([source_temp, target_temp])
 
-#preprocessing
+
+###############################################################################
+#PREPROCESSING
+###############################################################################
 #voxelization (down sampline), estimation of normal vectors, and geometric 
 #feature extration
 def preprocess_point_cloud(pcd, voxel_size):
@@ -43,8 +50,12 @@ def preprocess_point_cloud(pcd, voxel_size):
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
     return pcd_down, pcd_fpfh
 
-
+###############################################################################
+#REGISTER POINT CLOUDS AND OBTAIN TRANSFORMATION
+###############################################################################
 #registration of source onto target
+#source is what will be moving
+#target is what we are wanting to match to
 def getRegistration(source, target, method = "point2point", voxel_size = 2, iters = 30):
     #validating arguements
     if not method == "point2point" and not method == "point2plane":
@@ -127,21 +138,6 @@ def getRegistration(source, target, method = "point2point", voxel_size = 2, iter
 # #plot registered point clouds
 # monochromePlot(u058_12Trans, u058_01)
 
-# #applying the transformation to the file as a mesh and then exporting
-# #i will also load in the target as a mesh just to show that the alignment
-# #process works
-# u058_12Mesh = o3d.io.read_triangle_mesh("pat058u_12.ply")
-# u058_01Mesh = o3d.io.read_triangle_mesh("pat058u_01.ply")
-# o3d.visualization.draw_geometries([u058_12Mesh, u058_01Mesh])
-# monochromePlot(u058_12Mesh, u058_01Mesh)
-# #now we apply the transformation that was calculated for the point cloud
-# u058_12MeshTrans = u058_12Mesh.transform(reg01_12.transformation)
-# o3d.visualization.draw_geometries([u058_12MeshTrans, u058_01Mesh])
-# monochromePlot(u058_12MeshTrans, u058_01Mesh)
-# #then it can be exported
-# os.chdir("K:/testDir")
-# o3d.io.write_triangle_mesh("regiOutTest.ply", u058_12MeshTrans, write_ascii = True)
-
 #ILLUSTRATION
 # #looking at how this is effected by various parameters
 # #calculate registration
@@ -157,4 +153,66 @@ def getRegistration(source, target, method = "point2point", voxel_size = 2, iter
 # reg200 = getRegistration(source = u058_12, target=u058_01, iters = 200)
 # print(reg200)    
 # #iterations seem to not have a large impac
+
+
+###############################################################################
+#APPLY TRANSFORMATION AND EXPORT
+###############################################################################
+#the above transformation is done on the point cloud, we want to apply it to the 
+#mesh. open3d wont let you export face colors, trimesh will with some massaging
+#trimesh is a bit more editable and user friendly than open3d
+#there are also registration methods in trimesh that would be good to investigate
+#but for the moment I am just going to stick with using the registration I have
+#previously constructed and using trimesh to export
+#https://trimesh.org/trimesh.registration.html
+
+#inFile, the path to the 
+def registerAndExport(inFile, outFile, trans):
+    
+    #read in mesh
+    mesh = trimesh.load(inFile)
+    
+    #apply transformation
+    meshTrans = mesh.apply_transform(trans)
+    
+    #attach face colors in a way that trimesh will export
+    colors = meshTrans.visual.face_colors.astype(np.uint8)
+    meshTrans.face_attributes["red"] = colors[:, 0]
+    meshTrans.face_attributes["green"] = colors[:, 1]
+    meshTrans.face_attributes["blue"] = colors[:, 2]
+    meshTrans.face_attributes["alpha"] = colors[:, 3]
+    
+    #export mesh
+    meshTrans.export(file_obj = outFile, file_type = "ply", encoding = "ascii")
+    
+    return True
+
+
+#EXAMPLE WORKFLOW
+#the rme scans still need a bit of massaging into the correct format before they
+#can be used in this process. for now we will register a teeth3ds scan to an iosseg
+#scan (the will not line up exactly). Iosseg is the target, teeth3ds is the source
+# #obtaining transformation
+# iossegPath = "K:/IOSSegData/clean/trainCleanU/007_U.ply"
+# teeth3dsPath = "K:/teeth3DS/scanData/upperPlyDecim016/00OMSZGW_UDecim016.ply"
+# targetCloud = o3d.io.read_point_cloud(iossegPath)
+# sourceCloud = o3d.io.read_point_cloud(teeth3dsPath)
+# reg = getRegistration(source = sourceCloud, target=targetCloud)
+# #register the meshes and export
+# os.chdir("K:/testDir/")
+# registerAndExport(inFile = teeth3dsPath, outFile = "registerTest.ply", trans = reg.transformation)
+# #check registration via o3d meshes and monochrome plot
+# mesh1 = o3d.io.read_triangle_mesh("registerTest.ply")
+# mesh2 = o3d.io.read_triangle_mesh(iossegPath)
+# monochromePlot(mesh1, mesh2)
+# #check coloring
+# import sys
+# sys.path.append("Y:/dissModels/fastTgcnVersions/tools")
+# import plyFunctions as pf
+# pf.readAndPlot("registerTest.ply", "U")
+# pf.readAndPlot(iossegPath, "U")
+
+
+
+
     
