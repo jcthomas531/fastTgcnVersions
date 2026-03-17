e70eccb2e0077e84c5d339ab6cd982d413212c3b
J C C Thomas <jthomas48@argon-itf-login-3.hpc>
2026-03-17 11:48:34 -0500

    was using from scratch training function before which is wrong, updated to warmstart function

--- fastTgcnEasy/trainWarmstartRunner.py
+++ fastTgcnEasy/trainWarmstartRunner.py
@@ -1,19 +1,19 @@
 
 #
-runNote = "warmstartTest3, half and half base and no base teeth3ds scans, oriented like iosseg, 100 epochs, upper arch"
+runNote = "warmstartTest4_2, same test data as model trained on, new train data, 30 registered teeth3ds, 15 with base 15 without"
 #
 
 
 print(runNote)
 #should alredy be in the proper working directory
-import train
-train.fastTgcnEasy(arch = "u",
-                   testPath = "/Shared/gb_lss/Thomas/testDir/warmstartTestDataReg/test",
-                   trainPath = "/Shared/gb_lss/Thomas/testDir/warmstartTestDataReg/train",
+import trainWarmstart as tws
+tws.fastTgcnWarm(arch = "u",
+                   testPath = "/Shared/gb_lss/Thomas/IOSSegData/clean/testCleanU",
+                   trainPath = "/Shared/gb_lss/Thomas/testDir/warmstartTestData/train",
                    batch_size = 1,
                    k = 32,
                    numWorkers = 8,
-                   epochs = 100)
+                   epochs = 101)
 print(runNote)
 
 
