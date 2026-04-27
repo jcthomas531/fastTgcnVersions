77601a52616b5106b942f8bd981ab1b5829082ab
J C C Thomas <jthomas48@argon-itf-login-4.hpc>
2026-04-27 15:32:10 -0500

    function for centroids of two scans and their differences

--- tools/plyFunctions.py
+++ tools/plyFunctions.py
@@ -354,16 +354,60 @@ def toothCentroids(face, vertex):
 
 
 
-#read in a file with PlyData and get the face and vertex data
+###############################################################################
+#GET CENTROIDS FOR TWO SCANS
+###############################################################################
+
+#scan1File, scan2File are file paths to segmented (and ideally registered) meshes
+def  twoScanCentroids(scan1File, scan2File):
+    
+    #hard coding arch to U, can change later
+    scan1 = readAndFormat(scan1File, arch = "U")
+    scan2 = readAndFormat(scan2File, arch = "U")
+    
+    #calculate each scans centroids
+    scan1Cent = toothCentroids(face = scan1["face"], vertex = scan1["vert"])
+    scan2Cent = toothCentroids(face = scan2["face"], vertex = scan2["vert"])
+    
+    #give the two time points suffixes for their centroid location columns
+    toModify = scan1Cent.columns[range(1,4)] #this will be the same for both data frames
+    scan1Cent = scan1Cent.rename(columns={i: f"{i}_pre" for i in toModify})
+    scan2Cent = scan2Cent.rename(columns={i: f"{i}_post" for i in toModify})
+    
+    #it is possible based on incorrect segmentation that one scan will have a different
+    #number of segmented classes than another, i believe the arithmatic below will
+    #be able to handle missing values
+    
+    #since it is possible to have a different number of classes in the two data 
+    #frames, we must set the join up so that all of the classes are represented
+    if (len(scan1Cent) >=  len(scan1Cent)):
+        joinDirection = "left"
+    elif (len(scan1Cent) <  len(scan1Cent)):
+        joinDirection = "right"
+    else:
+        raise ValueError("Error in determining data frame lenghts prior to join")
+    
+    #join the two time points together
+    #pat055uBothCent = pat055u_01Cent.merge(pat055u_16Cent, on = "toothNum", how = "left")
+    scanBothCent = scan1Cent.merge(scan2Cent, on = "toothNum", how = joinDirection)
+    
+    #calculate the values for the distance vector between the two time points
+    scanBothCent["xDiff"] = scanBothCent["x_pre"] - scanBothCent["x_post"]
+    scanBothCent["yDiff"] = scanBothCent["y_pre"] - scanBothCent["y_post"]
+    scanBothCent["zDiff"] = scanBothCent["z_pre"] - scanBothCent["z_post"]
+    
+    #find the l2 norm of the distance vector
+    scanBothCent["l2Norm"] = (scanBothCent["xDiff"]**2 + scanBothCent["yDiff"]**2 + scanBothCent["zDiff"]**2) ** (1/2)
+    
+    #unit vector values for the distance vector
+    scanBothCent["xDiffUnit"] = scanBothCent["xDiff"]/scanBothCent["l2Norm"]
+    scanBothCent["yDiffUnit"] = scanBothCent["yDiff"]/scanBothCent["l2Norm"]
+    scanBothCent["zDiffUnit"] = scanBothCent["zDiff"]/scanBothCent["l2Norm"]
+    
+    return scanBothCent
 
 
 
-
-
-
-
-
-#identify colors by tooth number
 
 
 
