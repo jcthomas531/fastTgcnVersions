dafe299e39336cf57e77a2644a7c83ef0c6b1452
J C C Thomas <jthomas48@argon-login-5.hpc>
2026-05-06 16:47:48 -0500

    menial run changes

--- prediction/ftePrediction.py
+++ prediction/ftePrediction.py
@@ -4,7 +4,7 @@ import fastTgcnEasyPredictFun as ftep
 
 
 
-ftep.fastTgcnEasyPredict(inDir = "/Shared/gb_lss/Thomas/iowaRme/testDir/segTestInput",
-                         outDir = "/Shared/gb_lss/Thomas/iowaRme/testDir/segTestOutput",
+ftep.fastTgcnEasyPredict(inDir = "/Shared/gb_lss/Thomas/iowaRme/preDelivAndFinalScans/finalScanU/dec016OriScans",
+                         outDir = "/Shared/gb_lss/Thomas/iowaRme/preDelivAndFinalScans/finalScanU/dec016Seg",
                          modelPath = "/Users/jthomas48/dissModels/intraoralSegmentation/fastTgcnEasy/modelOutputs/2026_04_21 fullT3dsDec016/checkpointsAndLogs/checkpoints/coordinate_140_0.939648.pth"
                          )
