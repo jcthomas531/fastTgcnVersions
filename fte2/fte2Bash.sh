b46f362254696af61bdc909a9f682e0ff19910d7
J C C Thomas <jthomas48@argon-login-6.hpc>
2026-07-13 08:46:31 -0500

    reorganization

--- fte2/fte2Bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l ngpus=3
-#$ -pe smp 32
-#$ -o outputFiles/$JOB_ID_fastTgcnEasyOut.o
-#$ -e outputFiles/$JOB_ID_fastTgcnEasyError.e
-
-
-
-apptainer exec ../../../containers/lorwyn.sif python trainRunner2.py
--- fte2/warm2Bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l ngpus=3
-#$ -pe smp 32
-#$ -o outputFiles/$JOB_ID_warmstartOut.o
-#$ -e outputFiles/$JOB_ID_warmstartError.e
-
-
-
-apptainer exec ../../../containers/lorwyn.sif python trainWarmstartRunner2.py
