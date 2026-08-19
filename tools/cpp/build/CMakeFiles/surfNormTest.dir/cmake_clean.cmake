95ed2a45734ccb9178e77f8f3a491941e69790d1
J C C Thomas <jthomas48@argon-login-5.hpc>
2026-08-19 13:04:31 -0500

    testing c++ local desc fun

---
+++ tools/cpp/build/CMakeFiles/surfNormTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/surfNormTest.dir/surfNormTest.cpp.o"
+  "CMakeFiles/surfNormTest.dir/surfNormTest.cpp.o.d"
+  "surfNormTest"
+  "surfNormTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/surfNormTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
